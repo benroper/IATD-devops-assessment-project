278cf4a440f15a6326176e28ad70d2f25ce31ed1
Benjamin Roper <roperben@hotmail.com>
2025-03-22 19:15:17 +1100

    removed redundant airline variable from utilities.js

--- src/utilities.js
+++ src/utilities.js
@@ -117,8 +117,6 @@ export function generateFlightId(airline) {
         return undefined;
     }
 
-    airline = airline;
-
     let digits = [];
     for (let i = 0; i < 3; i++) {
         digits[i] = Math.round(Math.random() * 9);
